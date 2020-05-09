a061583d226d81c530bad19fa6bd72a402344865
Vlad-404 <vmijat21@gmail.com>
2020-05-09 16:17:52 +0000

    Removed some unnecessary code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// show/hide elements
+
 $("#chose-category").click(function() {
   
   $("#welcome").addClass("hide");
@@ -8,7 +10,7 @@ $("#chose-category").click(function() {
 $(".btn-grid>.no-style").click(function() {
   
   $("#categories").addClass("hide");
-  $("#questions").removeClass("hide");
+  $("#question-wrapper").removeClass("hide");
   
 });
 
@@ -26,31 +28,6 @@ $("#again").click(function() {
   
 });
 
-
-$("#link-index").click(function() {
-  
-  $("#welcome").removeClass("hide");
-  $("categories").addClass("hide");
-  $("#questions").addClass("hide");
-  $("#victory").addClass("hide");  
-});
-
-$("#link-categories").click(function() {
-  
-  $("#welcome").addClass("hide");
-  $("categories").removeClass("hide");
-  $("#questions").addClass("hide");
-  $("#victory").addClass("hide");
-});
-
-$("#link-questions").click(function() {
-  
-  $("#welcome").addClass("hide");
-  $("categories").addClass("hide");
-  $("#questions").removeClass("hide");
-  $("#victory").addClass("hide");
-});
-
 $("#link-victory").click(function() {
   
   $("#welcome").addClass("hide");
@@ -58,3 +35,69 @@ $("#link-victory").click(function() {
   $("#questions").addClass("hide");
   $("#victory").removeClass("hide");
 });
+
+// variables
+
+const nextButton = document.getElementById("next-btn")
+const questionElement = document.getElementById("question")
+const answerButtons = document.getElementById("answers")
+
+let shuffledQuestions, currentQuestionIndex
+
+//console.log(questionElement.innerText)
+
+function startGame() {
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    setNextQuestion()
+}
+
+function setNextQuestion () {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classlist.add("hide")
+}
+
+function selectAnswer (e) {
+
+}
+
+// test questions
+
+const questions = [
+    {
+        question: "What is 1 + 1?",
+        answers: [
+            {text: "2", correct: true },
+            {text: "22", correct: false },
+            {text: "11", correct: false },
+            {text: "7", correct: false }
+        ]
+    },
+    {
+        question: "Why did the chicken cross the road?",
+        answers: [
+            {text: "To run away!", correct: false },
+            {text: "To get to the other side", correct: true },
+            {text: "Who cares?", correct: false },
+            {text: "7", correct: false }
+        ]
+    }
+]
