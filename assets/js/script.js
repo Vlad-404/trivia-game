f462dd0f7d7d576ebd380d8ba2e73f816f937434
Vlad-404 <vmijat21@gmail.com>
2020-05-09 13:12:36 +0000

    Added JS to temporary navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
-/*$("#chose-category").click(function() {
-  
-  $("#welcome").addClass(hide);
-
-  $("categories").removeClass(hide);
-  
-}); */
-
 $("#chose-category").click(function() {
   
   $("#welcome").addClass("hide");
@@ -34,52 +26,29 @@ $("#again").click(function() {
   
 });
 
-/*$("#next-btn").click(function() {
-  
-  $("#categories").addClass("hide");
-  $("#questions").removeClass("hide");
-  
-}); */
-
-/*
 
 $("#link-index").click(function() {
   
   $("#welcome").removeClass("hide");
-  $("categories").css("display", "none");
-  $("#questions").css("display", "none");
-  $("#victory").css("display", "none");
-  $("#link-index").css("display", "none");
-  $("#link-categories").removeClass("hide");
-  $("link-questions").removeClass("hide");
-  $("link-victory").removeClass("hide");
-  
+  $("categories").addClass("hide");
+  $("#questions").addClass("hide");
+  $("#victory").addClass("hide");  
 });
 
 $("#link-categories").click(function() {
   
-  $("#welcome").css("display", "none");
+  $("#welcome").addClass("hide");
   $("categories").removeClass("hide");
-  $("#questions").css("display", "none");
-  $("#victory").css("display", "none");
-  $("#link-index").removeClass("hide");
-  $("#link-categories").css("display", "none");
-  $("link-questions").removeClass("hide");
-  $("link-victory").removeClass("hide");
-  
+  $("#questions").addClass("hide");
+  $("#victory").addClass("hide");
 });
 
 $("#link-questions").click(function() {
   
-  $("#welcome").css("display", "none");
-  $("categories").css("display", "none");
+  $("#welcome").addClass("hide");
+  $("categories").addClass("hide");
   $("#questions").removeClass("hide");
-  $("#victory").css("display", "none");
-  $("#link-index").removeClass("hide");
-  $("#link-categories").removeClass("hide");
-  $("link-questions").css("display", "none");
-  $("link-victory").removeClass("hide");
-  
+  $("#victory").addClass("hide");
 });
 
 $("#link-victory").click(function() {
@@ -88,9 +57,4 @@ $("#link-victory").click(function() {
   $("categories").addClass("hide");
   $("#questions").addClass("hide");
   $("#victory").removeClass("hide");
-  $("#link-index").removeClass("hide");
-  $("#link-categories").removeClass("hide");
-  $("link-questions").removeClass("hide");
-  $("link-victory").addClass("hide");
-  
-}); */
+});
