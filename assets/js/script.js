2b9d1a9246502f9adbf5611f7c37e99db96795e5
Vlad-404 <vmijat21@gmail.com>
2020-05-16 14:23:33 +0000

    Added container for loading screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// show/hide elements
+// Show/hide elements
 
 $("#chose-category").click(function() {
   
@@ -47,6 +47,135 @@ $("#link-victory").click(function() {
 });
 
 // VARIABLES
+
+const questionWrapper = document.getElementById("question-wrapper")
+
+const questionElement = document.getElementById("question")
+
+const restartButton = document.getElementById("restart-btn")
+
+const nextButton = document.getElementById('next-btn')
+
+const timerButton = document.getElementById("counter-btn")
+
+const answerButtonsElement = document.getElementById("answers")
+
+const controllButtons = document.getElementById("controlls")
+
+const categoryButtons = document.getElementsByClassName("category-btn")
+
+let shuffledQuestions, currentQuestionIndex  // shuffles the questions and sets the index on the current question
+
+// FUNCTIONS
+
+// Starts the game with random question and initiates setNextQuestion function
+function startGame() {
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    setNextQuestion()
+}
+
+// Sets the next question randomly
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+// Picks the next question and presents the answers
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn","btn-scaled")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        } else if (answer.false) {
+            wrongAnswer()
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+// Countdown timer
+function setTimer() {
+    let timeleft = 5;
+    let countdownTimer = setInterval(function() {
+  
+        if(timeleft <= 0) {
+            clearInterval(countdownTimer);
+            timerButton.innerHTML = "Time's Up!"
+            timerButton.classList.add("wrong")
+            timerButton.addEventListener("click", questionsToCategories)
+        } else {
+            document.getElementById("counter-btn").innerHTML = timeleft + " s";
+        }
+        timeleft -= 1;
+    }, 1000);
+}
+
+// Go back to category selection after failed answer or timed out
+function wrongAnswer () {
+    timerButton.classList.add("hide")
+    restartButton.classList.remove("hide")
+    restartButton.addEventListener("click", questionsToCategories)
+}
+
+function questionsToCategories() {
+    const categoriesCard = document.getElementById("categories")
+    questionWrapper.classList.add("hide")
+    categoriesCard.classList.remove("hide")
+    document.body.classList.remove("background-blurry")
+    document.body.classList.add("index-image")
+    clearStatusClass(timerButton)
+}
+
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion
+})
+
+// Resets the question screen to prepare it for new question
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+// Checks if the answer is correct or not and adds apropriate class
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide")
+    } else {
+        restartButton.classList.remove("hide")
+        timerButton.classList.add("hide")
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+}
+
+// Questions
 
 const questions = [
     {
@@ -150,154 +279,3 @@ function apiTranslator(data) {
 createAnswers(results);
 
 */
-const questionWrapper = document.getElementById("question-wrapper")
-
-const questionElement = document.getElementById("question")
-
-const restartButton = document.getElementById("restart-btn")
-
-const nextButton = document.getElementById('next-btn')
-
-const timerButton = document.getElementById("counter-btn")
-
-const answerButtonsElement = document.getElementById("answers")
-
-const controllButtons = document.getElementById("controlls")
-
-const categoryButtons = document.getElementsByClassName("category-btn")
-
-let shuffledQuestions, currentQuestionIndex  // shuffles the questions and sets the index on the current question
-
-//restartButton.addEventListener("click", startGame, setTimer)
-//categoryButtons.addEventListener("click", startGame, setTimer)
-
-// FUNCTIONS
-
-// Starts the game with random question and initiates setNextQuestion function
-function startGame() {
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    setNextQuestion()
-}
-
-// Sets the next question randomly
-function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-// Picks the next question and presents the answers
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("btn","btn-scaled")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        } else if (answer.false) {
-            incorrectAnswer()
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-}
-
-// Countdown timer
-function setTimer() {
-    let timeleft = 5;
-    let countdownTimer = setInterval(function() {
-  
-        if(timeleft <= 0) {
-            clearInterval(countdownTimer);
-            timerButton.innerHTML = "Time's Up!"
-            timerButton.classList.add("wrong")
-            timerButton.addEventListener("click", questionsToCategories)
-        } else {
-            document.getElementById("counter-btn").innerHTML = timeleft + " s";
-        }
-        timeleft -= 1;
-    }, 1000);
-}
-/*
-set timer with creating and deleting a button
-function setTimer() {
-    let createTimer = document.createElement("button")
-    
-    createTimer.innerHTML = setInterval(function(){
-        let timeleft = 5;
-        if(timeleft <= 0){
-        clearInterval(countdownTimer);
-        document.getElementById("counter-btn").innerHTML = "Time's up!";
-        wrongAnswer()
-        } else {
-        document.getElementById("counter-btn").innerHTML = timeleft + " s";
-            }
-        timeleft -= 1;
-    }, 1000);
-
-    let controllArea = document.getElementById("controlls")
-    controllArea.appendChild(createTimer)
-    createTimer.classList.add("btn", "btn-scaled")
-
-}*/
-
-// Go back to category selection after failed answer or timed out
-function incorrectAnswer () {
-    timerButton.classList.add("hide")
-    restartButton.classList.remove("hide")
-    restartButton.addEventListener("click", questionsToCategories)
-}
-
-function questionsToCategories() {
-    const categoriesCard = document.getElementById("categories")
-    questionWrapper.classList.add("hide")
-    categoriesCard.classList.remove("hide")
-    document.body.classList.remove("background-blurry")
-    document.body.classList.add("index-image")
-    clearStatusClass(timerButton)
-}
-
-nextButton.addEventListener("click", () => {
-    currentQuestionIndex++
-    setNextQuestion
-})
-
-// Resets the question screen to prepare it for new question
-function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add("hide")
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
-}
-
-// Checks if the answer is correct or not and adds apropriate class
-function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove("hide")
-    } else {
-        restartButton.classList.remove("hide")
-        timerButton.classList.add("hide")
-    }
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add("correct")
-    } else {
-        element.classList.add("wrong")
-    }
-}
-
-function clearStatusClass(element) {
-    element.classList.remove("correct")
-    element.classList.remove("wrong")
-}
