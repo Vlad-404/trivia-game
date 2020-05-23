e52c015b7f89b2d3721c086b983c669d564e5bde
Vlad-404 <vmijat21@gmail.com>
2020-05-23 14:50:44 +0000

    Succesfully implemented setQuestion to await for the fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,8 @@ const categoryButtons = document.getElementsByClassName("category-btn")
 
 const answerButtons = document.getElementsByClassName("answer")
 
+const loadingScreen = document.getElementById("loading-screen")
+
 // NAVIGATION
 
 $("#category-selection").click(function() {
@@ -27,23 +29,17 @@ $("#category-selection").click(function() {
 });
 
 $(".category-btn").click(function() {
-  
   $("#categories").addClass("hide");
-  $("body").removeClass("index-image").addClass("background-blurry");
-  $("#question-wrapper").removeClass("hide");
-  $("#link-victory").removeClass("hide");
-  $(".answers-counter").removeClass("hide");
-  $("#timer-btn").removeClass("hide");
-  //clearInterval(setTimer); ?
   showQuestion();
+  setTimer();
 });
 
 $("next-btn").click(function() {
-    $("next-btn").toggleClass("hide");
-    $("timer-btn").toggleClass("hide");
-    //clearStatusClass(document.body);
+    $("next-btn").addClass("hide");
+    $("timer-btn").removeClass("hide");
+    clearStatusClass(document.body);
+    clearStatusClass(timerButton);
     setTimer();
-    //fetchQuestionsGeneral();
 });
 
 $("#again").click(function() {
@@ -64,13 +60,11 @@ $("#link-victory").click(function() {
   $("body").removeClass("background-blurry").addClass("index-image");
 });
 
+// FUNCTIONS
 
-
-// API 
- 
-// Gets the questions
+// Function that uses API to get questions and anwers and makes them usable by the game
 function fetchQuestionsGeneral() {
-    fetch("https://opentdb.com/api.php?amount=1&type=multiple")
+    return fetch("https://opentdb.com/api.php?amount=1&type=multiple")
         .then(results => {
             if (results.ok) {
                 console.log("Questions retrieved!")
@@ -79,10 +73,13 @@ function fetchQuestionsGeneral() {
             }
             return results.json()
         })
+        .then(results => {
+            return results
+        })
         .then(data => {
             console.log(data)
             formattedQuestion = data.results[0].question
-            //questionElement.innerText = formattedQuestion
+            /*questionElement.innerText = formattedQuestion
             
             
             const formattedAnswers = []
@@ -94,82 +91,28 @@ function fetchQuestionsGeneral() {
             
             formattedAnswers.push(correctAnswer)
             console.log("Formatted", formattedAnswers)
-            /*
-            answerButtons.innerText.forEach(answer => {
-                const button = document.createElement("button")
-                button.innerText = shuffledAnswers
-                button.classList.add("btn","btn-scaled", "answer")
-                if (answer.correctAnswer) {
-                    button.dataset.correct = answer.correct
-                } else {
-                    wrongAnswer()
-                }
-                button.addEventListener("click", selectAnswer)
-            }) }
-            
-            function shuffledAnswers(formattedAnswers) {
-                let currentIndex = formattedAnswers.length, temporaryValue, randomIndex;
-
-                while (0 !== currentIndex) {
-                    randomIndex = Math.Floor(Math.random() * currentIndex);
-                    currentIndex -= 1;
-
-                    temporaryValue = formattedAnswers[currentIndex];
-                    formattedAnswers[currentIndex] = formattedAnswers[randomIndex];
-                    formattedAnswers[randomIndex] = temporaryValue;
-                }
-                return formattedAnswers;
-            }*/
-            
             
             console.log(incorrectAnswers)
             //console.log(formattedAnswers)
-
+            */
             console.log(formattedQuestion)
             return formattedQuestion
         })
-        
-        /*.then(loadedQuestions => {
-            console.log(loadedQuestions.results);
-            questions = loadedQuestions.results.map(loadedQuestion => {
-            const formattedQuestion = {
-                question: loadedQuestion.question
-            };
-
-            const answerChoices = [...loadedQuestion.incorrect_answers];
-
-            answerChoices.forEach((choice, index) => {
-                formattedQuestion["choice" + (index + 1)] = choice;
-            });
-
-            console.log(formattedQuestion);
-            return formattedQuestion;
-            });
-        })*/
         .catch(err => {
             console.error(err);
         });
+        
 }
 
-//questionElement.innerText = formattedQuestion
-
-// FUNCTIONS
-
 // Picks the next question and presents the answers
-function showQuestion() {
-    questionElement.innerHTML = formattedQuestion
-    /*question.answers.forEach(answer => {
-        const button = document.createElement("button")
-        button.innerText = answer.choice
-        button.classList.add("btn","btn-scaled", "answer")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        } else if (answer.false) {
-            wrongAnswer()
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })*/
+async function showQuestion() {
+    $("body").removeClass("index-image").addClass("background-blurry");
+    $("#question-wrapper").removeClass("hide");
+    $("#link-victory").removeClass("hide");
+    $(".answers-counter").removeClass("hide");
+    $("#timer-btn").removeClass("hide");
+    const formattedQuestion = await fetchQuestionsGeneral();
+    questionElement.innerHTML = formattedQuestion;
 }
 
 // Countdown timer
@@ -181,7 +124,10 @@ function setTimer() {
         clearInterval(countdownTimer);
         document.getElementById("timer-btn").innerHTML = "Time's up!";
         timerButton.classList.add("wrong")
-        timerButton.addEventListener("click", questionsToCategories)
+        timerButton.addEventListener("click", function(){
+        clearStatusClass(timerButton);
+        questionsToCategories();
+        })
     } else {
         document.getElementById("timer-btn").innerHTML = timeleft + " s";
     }
@@ -234,78 +180,3 @@ function clearStatusClass(element) {
     element.classList.remove("correct")
     element.classList.remove("wrong")
 }
-
-
-/*function apiTranslator(data) {
-  let answer = {
-    text: "",
-    correct: true,
-  };
-  answers.text = data.results.correct_answer;
-  question.push(answer);
-  data.incorrect_answers.forEach((val) => {
-    answer.text = val;
-    answer.correct = false;
-  });
-  question.push(answer);
-}*/
-
-/*
-function apiTranslator(data) {
-  let answer = {
-    text: "",
-    correct: true,
-  };
-  answers.text = data.correct_answer;
-  question.push(answer);
-  data.incorrect_answers.forEach((val) => {
-    answer.text = val;
-    answer.correct = false;
-  });
-  question.push(answer);
-} */
-
-
-// Questions
-
-/*
-const question = [
-  {
-    question: "What is 1 + 1?",
-    answers: [
-      { text: "2", correct: true },
-      { text: "22", correct: false },
-      { text: "11", correct: false },
-      { text: "7", correct: false },
-      // added values
-	  { text: "Neville Chamberlain", correct: true },
-      { text: "Clement Attlee", correct: false },
-    ],
-},
-];
-const results = [
-	{
-		category: "Politics",
-		type: "multiple",
-		difficulty: "medium",
-		question: "Who was the British Prime Minister at the outbreak of the Second World War?",
-		correct_answer: "Neville Chamberlain",
-		incorrect_answers: ["Clement Attlee", "Winston Churchill", "Stanley Baldwin"],
-	},
-];
-function apiTranslator(data) {
-  let answer = {
-    text: "",
-    correct: true,
-  };
-  answers.text = data.correct_answer;
-  question.push(answer);
-  data.incorrect_answers.forEach((val) => {
-    answer.text = val;
-    answer.correct = false;
-  });
-  question.push(answer);
-}
-createAnswers(results);
-
-*/
